21058168ba7559c387255a5d11e1254be6975b77
Maureen <isagimaureen@gmail.com>
2022-09-21 10:05:17 -0700

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,22 +7,12 @@
 
 void print_number(int n)
 {
-int divisor = 1, i, resp;
-
-if (n < 0)
-{
-	_putchar('-');
-	n *= -1;
-}
-
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-
-}
-
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
 }
