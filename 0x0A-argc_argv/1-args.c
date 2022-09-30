ee93e947df2cbd49bf586fa9c7048b2bf19ca90a
Maureen <isagimaureen@gmail.com>
2022-09-30 10:23:34 -0700

    Task 1updt

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,8 +5,9 @@
  * @argv: Argument vector
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("argc = %d\n", argc);
-	return 0;
+	printf("%d\n", argc - 1);
+
+	return (0);
 }
